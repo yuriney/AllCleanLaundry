9f47ef60f58f3eafc3973ad70e33bedf034ce60a
yuriMartin <yurimn14@outlook.com>
2025-05-12 16:46:45 -0300

    ADDED: SpringBoot Project with Spring Web and lombok

---
+++ AllClean-backend/laundryservice/src/main/java/com/laundry/laundryservice/LaundryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.laundry.laundryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryserviceApplication.class, args);
+	}
+
+}
---
+++ AllClean-backend/laundryservice/src/test/java/com/laundry/laundryservice/LaundryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laundry.laundryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
