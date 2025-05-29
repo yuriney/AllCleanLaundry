9f887700937e6acda5dc56e5925f9afcf070baa9
yuriMartin <yurimn14@outlook.com>
2025-05-29 09:57:43 -0300

    Update on class Cors to receive the correct frontend's URL

--- AllClean-backend/laundryservice/src/main/java/com/laundry/laundryservice/config/CorsConfig.java
+++ AllClean-backend/laundryservice/src/main/java/com/laundry/laundryservice/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://allcleanlaundry.onrender.com/")
                 .allowedMethods("*");
     }
 }
