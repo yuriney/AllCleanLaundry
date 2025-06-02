d6a5e29c2bffbfe3e0c851a5112b3206bafbe4eb
yuriMartin <yurimn14@outlook.com>
2025-06-02 16:14:00 -0300

    Fix Cors urls that are allowed

--- AllClean-backend/laundryservice/src/main/java/com/laundry/laundryservice/config/CorsConfig.java
+++ AllClean-backend/laundryservice/src/main/java/com/laundry/laundryservice/config/CorsConfig.java
@@ -1,8 +1,8 @@
 package com.laundry.laundryservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -10,8 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins( "https://allcleanlaundry.onrender.com",
-                        "https://allcleanlaundry-production.up.railway.app")
+                .allowedOrigins(
+                        "https://allcleanlaundry.onrender.com",
+                        "https://allcleanlaundry-production.up.railway.app"
+                )
                 .allowedMethods("*");
     }
 }
